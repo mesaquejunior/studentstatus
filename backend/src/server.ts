43c8846ff8a6145d73a93b23d408d188fefedaed
Mesaque Botelho Junior <mesaquejunior@gmail.com>
2020-12-16 10:27:21 -0300

    Created frontend project with initial configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,3 +3,5 @@ import app from './app'
 app.listen(3333)
 
 // https://csvjson.com/csv2json
+// students mKUpUtsQYJ7rnZ2w
+// root pPJuSYVUbeZW3QQ6
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return(
+    <header>Home</header>
+  )
+}
+
+export default Home;
